cd3c544b3dea96e283ac8b825cadfa2189bd9640
Ryuya <volley07@icloud.com>
2019-07-10 21:59:47 +0900

    Player.java is updated

--- Player.java
+++ Player.java
@@ -3,6 +3,10 @@ public class Player {
     private String name;
 
     public Player() {
-        
+        System.out.println("What is your name?");
+        System.out.print("> ");
+        var scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
