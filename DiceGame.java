90dbd2c0dba8d43c5c1359da48d809127005d227
Ryuya <volley07@icloud.com>
2019-07-10 22:16:51 +0900

    DiceGame.java is updated

--- DiceGame.java
+++ DiceGame.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class DiceGame {
     private Die die1, die2;
+    private final int luckuNumber;
 
     public DiceGame() {
+        var rand = new Random();
+        luckuNumber = rand.nextInt(12) + 1;
         die1 = new Die();
         die2 = new Die();
     }
@@ -19,9 +24,22 @@ public class DiceGame {
         System.out.println( "Total value: " +  total);
 
         if( total > 7 ) {
-            System.out.println("You won");
+            System.out.println( p.getName() + " won!" );
         } else {
-            System.out.println("You lost");
+            System.out.println( p.getName() + " lost." );
+            if( total == luckuNumber ) {
+                System.out.println( "\nOops, you seem lucky!!" );
+                die1.roll();
+                die2.roll();
+                total = die1.getFaceValue() + die2.getFaceValue();
+                System.out.println( "Total value: " +  total);
+    
+                if( total > 7 ) {
+                    System.out.println( p.getName() + " won!" );
+                } else {
+                    System.out.println( p.getName() + " lost." );
+                }
+            }
         }
     }
 
